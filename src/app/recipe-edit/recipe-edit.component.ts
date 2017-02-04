09a025391150e8547705d5676b2f6e287a51ff16
Scott Kornblatt <skornblatt@rtm.com>
2017-02-04 08:29:50 -0500

    began work on data driven form for recipes

--- src/app/recipe-edit/recipe-edit.component.ts
+++ src/app/recipe-edit/recipe-edit.component.ts
@@ -1,14 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Rx';
+
+import { RecipeService } from '../recipes/recipe.service';
 
 @Component({
   selector: 'rb-recipe-edit',
   templateUrl: './recipe-edit.component.html'
 })
-export class RecipeEditComponent implements OnInit {
+export class RecipeEditComponent implements OnInit, OnDestroy {
+  private recipeIndex: number;
+  private subscription: Subscription;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private recipeService: RecipeService) {
 
-  ngOnInit() {
   }
 
+  ngOnInit() {
+    let isNew = true;
+    this.subscription = this.route.params.subscribe(
+      (params: any) => {
+        this.recipeIndex = +params['id'];
+        if (params.hasOwnProperty('id')) {
+          isNew = false;
+          this.recipeIndex = +params['id'];
+        } else {
+          isNew = true;
+        }
+      }
+    );
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
+  }
 }
